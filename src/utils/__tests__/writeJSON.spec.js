cf8ac9dab6d2e99c8f82ab1599da92e1952ea553
Sergi Romeu <sergi.romeu@travelport.com>
2019-02-25 16:14:13 +0100

    refactor: improve code

--- src/utils/__tests__/writeJSON.spec.js
+++ src/utils/__tests__/writeJSON.spec.js
@@ -4,8 +4,10 @@ const fs = require('fs');
 const { writeJSON, pathToUsers } = require('..');
 
 describe('writeJSON', () => {
-  const fsSpy = sinon.stub(fs, 'writeFileSync');
-  const content = { name: 'test' };
+  const fsSpy = sinon.stub(fs, 'writeFileSync').callsFake(() => {
+    return;
+  });
+  const content = { users: [{ email: 'test', password: '123' }] };
 
   it('should call fs with the expected parameters', () => {
     writeJSON(content);
